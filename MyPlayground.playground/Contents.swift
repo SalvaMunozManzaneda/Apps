44b0d31fa10718fe00515f42d527808a306e7c14
Salvador Muñoz Manzaneda <salva17y2@gmail.com>
2017-12-17 12:01:40 +0100

    Desenvolver Optionals Clases y subclases
    unwrapping optionals y haciendo algo de clases y herencia.

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -68,13 +68,42 @@ var ageInt = Int(stringAge) //aqui lo envuelves como un int que era string "wrap
 print(ageInt)//esto es un opcional, si le pasas un numero funciona, si le pasas al string una palabra le guarda un nil.
 print(ageInt!)//el simbolo de exclamacion sirve para quitarle el opcional y dejarlo como variable normal (int string...) "unwrapping"
 
+//un nil no puede mostrarse por pantalla, hay que "desenvolverlo" (!)
+var gominolaFav : String? = nil
 
+if gominolaFav != nil {//si es distinto de nil, se puede mostrar
+    print(gominolaFav!)
+}
+//otra opcion
+if let favorita = gominolaFav { //solo guardas en favorita si es algo "desenvuelto", si es opcional no se guarda (le faltaria pasarlo a "unwrapping" (!)
+    print(favorita)
+}
 
+//Clases y Herencia
+//clase
+class Animal {
+    var name = ""
+    var age = 0
+}
 
+class Dog : Animal {//dog hereda los atributos de animal.
+    var colorPelo = "blanco con manchas"
+}
 
+var miPerro = Dog()
+miPerro.age = 12//atributo de Animal()
+miPerro.colorPelo = "marron" //atributo de Dog()
 
+var animalAleatorio : Animal = Dog() //asi no funcionaria, solo le daria atributos de animal
+let unAnimal = animalAleatorio as! Dog //aqui "forzamos" a que sea un Dog() (Animal aleatorio ya es de la clase Animal() y unAnimal es animalAleatorio "forzado" a Dog()
 
+class Monkey : Animal {
+    var hands = true
+}
 
+if let otroAnimal = animalAleatorio as? Dog {//Entramos dentro del if si animalAleatorio funciona como Dog(), si cambiaramos a Monkey() no funcionaria, ya que arriba decimos que es un Dog()
+    print(otroAnimal.colorPelo)
+}
 
 
 
