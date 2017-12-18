8749197eaa5a32ca9f637cc215321ddf319114b2
Salvador Muñoz Manzaneda <salva17y2@gmail.com>
2017-12-18 13:09:08 +0100

    añadido CoreData
    Aun no refresca la pantalla principal una vez guardados los cambios en el core.

--- ToDo List/ToDo List/AddToDoViewController.swift
+++ ToDo List/ToDo List/AddToDoViewController.swift
@@ -23,13 +23,30 @@ class AddToDoViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     @IBAction func addTapped(_ sender: Any) {
-        let toDo = ToDo()
+        
+        /*
+         let toDo = ToDo()
         toDo.name = tituloText.text!
         toDo.important = importantSwich.isOn
         
         previusVC.toDos.append(toDo) //añadimos el objeto que creamos justo encima al array de la otra clase toDos
         previusVC.tableView.reloadData() //le decimos a la clase que tiene previusVC que vuelva a leer los datos ya que hemos introducido nuevos pero tiene cargados los antiguos.
         navigationController?.popViewController(animated: true)//volvemos automaticamente a la pantalla anterior | el true es para que haga una vuelta atras con animacion
+         */
+        
+        
+        //saca el contexto (aun no se muy bien que es)
+       if let context = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext {
+        
+            let toDo = ToDoCoreData(entity: ToDoCoreData.entity(), insertInto: context)
+        
+            if let titleText = tituloText.text {//asegurar que es un string (arriba hay un (!) para forzarlo
+                toDo.name = titleText
+                toDo.important = importantSwich.isOn
+            }
+            try? context.save() //guarda en el "CoreData"
+        
+            navigationController?.popViewController(animated: true)
+        }
     }
-    
 }
